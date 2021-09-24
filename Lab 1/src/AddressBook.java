bf8e3d3499853d3dc2176bdcabece8100c99834b
Hamza <Hamzazafar@cmail.carleton.ca>
2021-09-24 12:05:30 -0400

    add a small print statement to AddressBook.java

--- Lab 1/src/AddressBook.java
+++ Lab 1/src/AddressBook.java
@@ -22,7 +22,6 @@ public class AddressBook
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
-
-
+        System.out.println("This is a change");
     }
 }
